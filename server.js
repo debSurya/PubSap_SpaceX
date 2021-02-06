4726ba89bd804a9594e0c9ed3be724c6ec5e0166
suryaDeb <deb.surya1011011@gmail.com>
2021-02-06 17:24:32 +0530

    updated app dist path for heroku

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/SpaceXApp'));
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/SpaceXApp/index.html'));
+app.use(express.static(__dirname + '/dist/SpaceXApp/browser'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/SpaceXApp/browser/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
